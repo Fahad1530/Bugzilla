12a60f61921c4b53fd5250d38eada634127f2a51
Fahad1530 <fahad.murtaza@devsinc.com>
2022-06-27 13:50:02 +0500

    homepage for all 3 users

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,13 +1,10 @@
 class ProjectsController < ApplicationController
-  before_action :set_project, only: %i[ show edit update destroy  ]
-  before_action :authenticate_user!, except: [:index, :show]
-  before_action :correct_user, only: [:edit, :update, :destroy]
-
+  before_action :set_project, only: %i[show edit update destroy]
+  before_action :authenticate_user!, except: %i[index show]
+  before_action :correct_user, only: %i[edit update destroy]
 
   def index
-    if current_user
-      @projects = current_user.projects.all
-    end
+    @projects = current_user.projects.all if current_user
   end
 
   def show
@@ -18,15 +15,14 @@ class ProjectsController < ApplicationController
     @project = current_user.projects.new
   end
 
-  def edit
-  end
+  def edit; end
 
   def create
     @project = Project.create(project_params)
     respond_to do |format|
       if @project.save
-        @project.workin_ons.create(user:current_user)
-        format.html { redirect_to project_url(@project), notice: "Project was successfully created." }
+        @project.workin_ons.create(user: current_user)
+        format.html { redirect_to project_url(@project), notice: 'Project was successfully created.' }
       else
         format.html { render :new, status: :unprocessable_entity }
       end
@@ -36,7 +32,7 @@ class ProjectsController < ApplicationController
   def update
     respond_to do |format|
       if @project.update(project_params)
-        format.html { redirect_to project_url(@project), notice: "Project was successfully updated." }
+        format.html { redirect_to project_url(@project), notice: 'Project was successfully updated.' }
       else
         format.html { render :edit, status: :unprocessable_entity }
       end
@@ -48,51 +44,48 @@ class ProjectsController < ApplicationController
     @project.destroy
 
     respond_to do |format|
-      format.html { redirect_to projects_url, notice: "Project was successfully destroyed." }
+      format.html { redirect_to projects_url, notice: 'Project was successfully destroyed.' }
     end
   end
 
   def correct_user
     @project = current_user.projects.find_by(id: params[:id])
-    redirect_to projects_path, notice: "Not Authorized to edit this project " if @project.nil?
+    redirect_to projects_path, notice: 'Not Authorized to edit this project ' if @project.nil?
   end
 
   def all_users
     @project = Project.find(params[:project_id])
     @users = @project.users
     @all_users = User.where.not(id: @project.users.ids)
-
   end
-
 
   def add_users
     @project = Project.find(params[:id])
-    if @project.users << User.find(params[:user_id])
-      flash[:project] = 'User added successfully'
-    else
-      flash[:project] = 'User could not be added'
-    end
+    flash[:project] = if @project.users << User.find(params[:user_id])
+                        'User added successfully'
+                      else
+                        'User could not be added'
+                      end
     redirect_to all_users_projects_path(@project)
   end
 
-   def remove_users
+  def remove_users
     @project = Project.find(params[:id])
-    if @project.users.delete(params[:user_id])
-      flash[:project] = 'User deleted successfully'
-    else
-      flash[:project] = 'User could not be deleted'
-    end
+    flash[:project] = if @project.users.delete(params[:user_id])
+                        'User deleted successfully'
+                      else
+                        'User could not be deleted'
+                      end
     redirect_to all_users_projects_path(@project)
   end
 
   private
 
-    def set_project
-      @project = Project.find(params[:id])
-    end
+  def set_project
+    @project = Project.find(params[:id])
+  end
 
-    def project_params
-      params.require(:project).permit(:title, :user_id)
-    end
-
+  def project_params
+    params.require(:project).permit(:title, :user_id)
+  end
 end
