4cabf7ac950be7c936d4accb0ab6d05f31a77c7a
Fahad1530 <fahad.murtaza@devsinc.com>
2022-06-27 18:21:28 +0500

    resolved the issue of enum word new

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -1,6 +1,6 @@
 class Bug < ApplicationRecord
   belongs_to :user
   belongs_to :project
-  enum status: { new: 0, started: 1, completed: 2 }
-  enum bug_type: { feature: 0, bug: 1 }
+  enum status: { fresh: 0, started: 1, completed: 2 }
+  enum bug_type: { feature: 0, bug_type: 1 }
 end
