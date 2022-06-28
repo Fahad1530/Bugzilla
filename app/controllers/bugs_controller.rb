7140f987f56eee5e0548da116f12e8304b4e4fe8
Fahad1530 <fahad.murtaza@devsinc.com>
2022-06-28 17:23:45 +0500

    added index page for bugs

--- app/controllers/bugs_controller.rb
+++ app/controllers/bugs_controller.rb
@@ -1,4 +1,11 @@
 class BugsController < ApplicationController
+  def index
+    @project = Project.find(params[:project_id])
+    @bug = @project.bugs
+
+  end
+
+
   def new
     @project = Project.find(params[:project_id])
     @bug = @project.bugs.build
@@ -9,6 +16,11 @@ class BugsController < ApplicationController
     @bug = @project.bugs.new(bug_params)
     @bug.user = current_user
     @bug.save
+    flash[:bug] = if @bug.save
+                    'Bug added successfully'
+                  else
+                    'Bug could not be added'
+                  end
     redirect_to projects_path
   end
 
