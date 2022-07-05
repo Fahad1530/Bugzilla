68c651bf7ca501c4d69260dfd0fc603c3cb499e8
Fahad1530 <fahad.murtaza@devsinc.com>
2022-07-05 22:29:30 +0500

    rubocop resolved all errors

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Bug < ApplicationRecord
-  belongs_to :user, foreign_key: 'creator_id'
+  belongs_to :user, foreign_key: 'creator_id', inverse_of: :bugs
   belongs_to :project
   enum status: { fresh: 0, started: 1, completed: 2 }
   enum bug_type: { feature: 0, bug_type: 1 }
