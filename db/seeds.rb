7f99b84e897a9f5aaa68b95c98a2fbe7fefa4958
Fahad1530 <fahad.murtaza@devsinc.com>
2022-07-06 13:11:56 +0500

    use of seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,21 @@
-# frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!([{
+  name: "Fahad",
+  email: "fahadmurtaza1530@gmail.com",
+  password: 123456,
+  role: "manager"
+
+},
+{
+  name: "ali",
+  email: "fahad.murtaza@devsinc.com",
+  role: "developer",
+  password: 123456
+
+},
+{
+  name: "ibrar",
+  email: "ibrarhussain0602@gmail.com",
+  role: "qa",
+  password: 123456
+}])
