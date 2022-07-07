ef3b55607d4ba6e2df6037d9970b5e058c60055a
Fahad1530 <fahad.murtaza@devsinc.com>
2022-07-07 12:42:57 +0500

    Resolved issue of seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,47 @@
+user1 = User.new(
+  name: 'Fahad',
+  email: 'fahadmurtaza1530@gmail.com',
+  password: 123_456,
+  role: 'manager'
+)
+user1.skip_confirmation!
+user1.save!
 
-User.create!([{
-  name: "Fahad",
-  email: "fahadmurtaza1530@gmail.com",
-  password: 123456,
-  role: "manager"
+user1.projects.create(
+  title: 'Fahad Project'
+)
 
-},
-{
-  name: "ali",
-  email: "fahad.murtaza@devsinc.com",
-  role: "developer",
-  password: 123456
+user1.projects.create(
+  title: 'Second Project'
+)
 
-},
-{
-  name: "ibrar",
-  email: "ibrarhussain0602@gmail.com",
-  role: "qa",
-  password: 123456
-}])
+user1.projects.create(
+  title: 'Third Project'
+)
 
-u = User.create(name: "Fahad",
-  email: "fahadmurtaza1530@gmail.com",
-  password: 123456,
-  role: "manager")
-  
+user1.projects.create(
+  title: 'Third Project'
+)
+
+user1.projects.create(
+  title: 'Third Project'
+)
+
+
+user2 = User.new(
+  name: 'ali',
+  email: 'fahad.murtaza@devsinc.com',
+  role: 'developer',
+  password: 123_456
+)
+user2.skip_confirmation!
+user2.save!
+
+user3 = User.new(
+  name: 'ibrar',
+  email: 'ibrarhussain0602@gmail.com',
+  role: 'qa',
+  password: 123_456
+)
+user3.skip_confirmation!
+user3.save!
