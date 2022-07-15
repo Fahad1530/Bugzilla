faf8febc90abf535e63a7f0f069905d5e6376dee
Fahad1530 <fahad.murtaza@devsinc.com>
2022-07-15 11:30:24 +0500

    proper messeges for rails

--- app/controllers/bugs_controller.rb
+++ app/controllers/bugs_controller.rb
@@ -3,6 +3,7 @@
 class BugsController < ApplicationController
   before_action :set_project, only: %i[new index create]
   after_action :authorize_bug, only: %i[index new create update]
+  before_action :developer_to_bug, only: %i[update]
 
   def index
     @bug = @project.bugs.all
@@ -15,18 +16,16 @@ class BugsController < ApplicationController
   def create
     @bug = @project.bugs.new(bug_params)
     @bug.user = current_user
-    respond_to do |format|
-      if @bug.save
+    if @bug.save
+      respond_to do |format|
         format.html { redirect_to project_bugs_path(@project.id), notice: 'bug was successfully created.' }
-      else
-        format.html { redirect_to new_project_bug_path(@project.id), notice: 'Vallidation Failed' }
       end
+    else
+      format.html { render :new, status: :unprocessable_entity }
     end
   end
 
   def update
-    @bug = Bug.find(params[:id])
-    @bug.developer_id = current_user.id
     if @bug.save
       respond_to do |format|
         format.html { redirect_to project_bugs_path, notice: 'bug assignment successfully' }
@@ -48,5 +47,10 @@ class BugsController < ApplicationController
 
   def authorize_bug
     authorize @bug
+  end
+
+  def developer_to_bug
+    @bug = Bug.find(params[:id])
+    @bug.developer_id = current_user.id
   end
 end
