b4f8d48b792df74e17ac0e4aace1747d967d7b06
Fahad1530 <fahad.murtaza@devsinc.com>
2022-08-01 13:12:40 +0500

    model association fixtures

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,9 +4,9 @@ class Project < ApplicationRecord
   has_many :project_users,  dependent: :destroy
   has_many :users, through: :project_users, dependent: :destroy
   has_many :project_developers, dependent: :destroy
-  has_many :developers, class_name: 'User', through: :project_developers, dependent: :destroy, source: :project
+  has_many :developers, class_name: 'User', through: :project_developers, dependent: :destroy, source: :user
   has_many :project_qas, dependent: :destroy
-  has_many :qas, class_name: 'User', through: :project_qas, dependent: :destroy, source: :project
+  has_many :qas, class_name: 'User', through: :project_qas, dependent: :destroy, source: :user
   has_one  :project_manager, dependent: :destroy
   has_one :manager, class_name: 'User', through: :project_manager, dependent: :destroy, source: :user
 
