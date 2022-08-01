177f8b3ba19bfc7a9e6af15742e07d37f246e164
Fahad1530 <fahad.murtaza@devsinc.com>
2022-08-01 13:38:16 +0500

    changes in production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,8 +16,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    user_name: 'fahad.murtaza@devsinc.com',
-    password: 'obtmcctectzrkexj',
+    user_name: Rails.application.credentials.gmail[:SENDMAIL_USERNAME],
+    password: Rails.application.credentials.gmail[:SENDMAIL_PASSWORD],
     domain: 'gmail.com',
     address: 'smtp.gmail.com',
     port: '587',
